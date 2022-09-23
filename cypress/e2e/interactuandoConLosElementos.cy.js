02f4bb534d83c51f2ac2441d1ef9476f0feb875e
Jennifer Rondón <rondon.jenni@gmail.com>
2022-09-23 08:07:00 -0400

    interactuando con los elementos checkboxes and radio buttons, input type text and others

--- cypress/e2e/interactuandoConLosElementos.cy.js
+++ cypress/e2e/interactuandoConLosElementos.cy.js
@@ -1,7 +1,7 @@
 describe('Interactuar con los elementos', ()=>{
-    beforeEach(()=>{
-        cy.visit('/buttons')
-    })
+    // beforeEach(()=>{
+    //     cy.visit('/buttons')
+    // })
 
     it('Click', ()=>{
         cy.get('button').eq(3).click()
@@ -25,9 +25,28 @@ describe('Interactuar con los elementos', ()=>{
       
     })
 
-    it.only('Click por posición', ()=>{
+    it('Click por posición', ()=>{
         cy.get('button').eq(3).parent().parent().click('topRight')
         cy.get('button').eq(3).parent().parent().click(5,60)
 
+    })
+
+    it('Input type text', ()=>{
+        cy.visit('/automation-practice-form')
+        cy.get('#firstName').type('Jennifer')
+        cy.get('#lastName').type('Rondón')
+        cy.get('#firstName').type('{selectAll}{backspace}')
+        cy.get('#firstName').type('Andrea')
+        cy.get('#firstName').clear()
+    })
+
+    it.only('Checkboxes and radio buttons', ()=>{
+        cy.visit('/automation-practice-form')
+        //cy.get('#gender-radio-1').click()
+        //cy.get('#gender-radio-1').click({force:true})
+        cy.get('label[for="gender-radio-1"]').click()
+        cy.get('label[for="hobbies-checkbox-1"]').click()
+
+        
     })
 })
